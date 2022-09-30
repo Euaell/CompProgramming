e60f28f34c98c3bdc7082b8aa4b0beed9022207b
Euael Eshete <euaelmeko@gmail.com>
2022-09-30 17:59:58 +0300

    Time: 560 ms (5.55%), Space: 48.8 MB (24.07%) - LeetHub

--- 848-shifting-letters/848-shifting-letters.cs
+++ 848-shifting-letters/848-shifting-letters.cs
@@ -1,8 +1,6 @@
 public class Solution {
     public string ShiftingLetters(string s, int[] shifts) {
         int n = s.Length;
-            
-        char[] chars = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
         int[] preF = new int[n + 1];
         for (int i = n - 1; i >= 0; i--)
@@ -11,8 +9,8 @@ public class Solution {
         char[] ans = new char[n];
 
         for (int i = 0; i < n; i++)
-            ans[i] = chars[(s[i] - 'a' + preF[i]) % 26];
+            ans[i] = (char)(((s[i] - 'a' + preF[i]) % 26) + 'a');
 
-        return string.Join("", ans);
+        return new String(ans);
     }
 }
