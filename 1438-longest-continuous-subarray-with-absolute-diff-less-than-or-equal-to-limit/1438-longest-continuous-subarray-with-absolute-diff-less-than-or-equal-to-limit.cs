c0cb7586681e9c53e8c36adfd29b7c0229679c5f
Euael Eshete <euaelmeko@gmail.com>
2022-10-04 20:22:58 +0300

    Time: 396 ms (49.06%), Space: 52.1 MB (37.74%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cs
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cs
@@ -1,32 +1,31 @@
 public class Solution {
-    private readonly KeyValuePair<int, int> _df = new KeyValuePair<int, int>(0, 0);
     public int LongestSubarray(int[] nums, int limit) {
         int n = nums.Length;
         
-        int ans = 0;
+        LinkedList<int> min = new();
+        LinkedList<int> max = new();
 
-        SortedDictionary<int, int> min = new();
-        SortedDictionary<int, int> max = new();
+        int ans = 0;
         
         int left = 0;
         for (int i = 0; i < n; i++)
         {
-            var elem = nums[i];
-            if (!min.ContainsKey(elem)) min.Add(elem, 0);
-            min[elem]++;
-            if (!max.ContainsKey(-elem)) max.Add(-elem, 0);
-            max[-elem]++;
+            while (max.Count > 0 && nums[i] > max.Last.Value) max.RemoveLast();
+            while (min.Count > 0 && nums[i] < min.Last.Value) min.RemoveLast();
+            
+            min.AddLast(nums[i]);
+            max.AddLast(nums[i]);
 
-            while (-max.FirstOrDefault(_df).Key - min.FirstOrDefault(_df).Key > limit)
+            while (max.First.Value - min.First.Value > limit)
             {
-                if (--min[nums[left]] == 0) min.Remove(nums[left]);
-                if (--max[-nums[left]] == 0) max.Remove(-nums[left]);
+                if (max.First.Value == nums[left]) max.RemoveFirst();
+                if (min.First.Value == nums[left]) min.RemoveFirst();
                 left++;
             }
-            
+
             ans = Math.Max(ans, i - left + 1);
         }
-        
+
         return ans;
     }
 }
