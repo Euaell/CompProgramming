0c7fca5632ff6708cd9e3f36d2c92cac988d5c8a
Euael Eshete <euaelmeko@gmail.com>
2022-10-04 17:20:20 +0300

    Time: 136 ms (98.02%), Space: 43.7 MB (5.94%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cs
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cs
@@ -3,14 +3,31 @@ public class Solution {
         int n = prices.Length;
         int[] ans = new int[n];
         
-        for (int i = 0; i < n; i++) {
+        // for (int i = 0; i < n; i++) {
+        //     int tmp = prices[i];
+        //     for (int j = i + 1; j < n; j++) 
+        //         if (prices[j] <= prices[i]) {
+        //             tmp -= prices[j];
+        //             break;
+        //         }
+        //     ans[i] = tmp;
+        // }
+        
+        
+        // Using monotonic stack
+        
+        Stack<int> stk = new(); 
+        
+        for (int i = n - 1; i >= 0; i--) {
             int tmp = prices[i];
-            for (int j = i + 1; j < n; j++) 
-                if (prices[j] <= prices[i]) {
-                    tmp -= prices[j];
-                    break;
-                }
+            
+            while (stk.Any() && prices[i] < stk.Peek()) 
+                stk.Pop();
+            
+            if (stk.Any()) tmp -= stk.Peek();
+            
             ans[i] = tmp;
+            stk.Push(prices[i]);
         }
         
         return ans;
