7b2d48c5cef2b20fc5264fc95832d5c3c75348cd
Euael Eshete <euaelmeko@gmail.com>
2022-10-04 15:38:32 +0300

    Time: 489 ms (17.86%), Space: 43.7 MB (28.57%) - LeetHub

---
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cs
@@ -0,0 +1,39 @@
+public class Solution {
+    
+    private (int, int)[] directions = {(1, 0), (0, 1), (-1, 0), (0, -1)};
+    
+    public int NearestExit(char[][] maze, int[] e) {
+        int n = maze.Length;
+        int m = maze[0].Length;
+        
+        var visited = new HashSet<(int, int)>();
+        var que = new Queue<(int, int)>();
+        
+        bool isValid((int, int) p) {
+            return p.Item1 < n && p.Item1 >= 0 && p.Item2 < m && p.Item2 >= 0 && maze[p.Item1][p.Item2] == '.' && !visited.Contains((p.Item1, p.Item2));
+        }
+
+        bool IsExit((int, int) p)
+        {
+            return p != (e[0], e[1]) && (p.Item1 == 0 || p.Item1 == n - 1 || p.Item2 == 0 || p.Item2 == m - 1);
+        }
+        
+        que.Enqueue((e[0], e[1]));
+        int ans = 0;
+        while (que.Any())
+        {
+            for (int size = que.Count; size > 0; size--)
+            {
+                var (row, col) = que.Dequeue();
+                if (visited.Contains((row, col))) continue;
+                visited.Add((row, col));
+                if (IsExit((row, col))) return ans;
+                foreach (var point in directions.Select(p => (p.Item1 + row, p.Item2 + col)))
+                    if (isValid(point)) que.Enqueue(point);
+            }
+            ans++;
+        }
+        
+        return -1;
+    }
+}
