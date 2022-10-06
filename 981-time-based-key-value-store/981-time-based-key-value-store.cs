205bfcf64b17d40aa2bcaf882727dffc0f9eea54
Euael Eshete <euaelmeko@gmail.com>
2022-10-06 11:29:30 +0300

    Time: 1374 ms (17.88%), Space: 113.1 MB (73.36%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.cs
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cs
@@ -1,27 +1,47 @@
 public class TimeMap {
-    private Dictionary<string, List<(int, string)>> dict;
+   Dictionary<string, List<(int, string)>> map;
+    
     public TimeMap() {
-        dict = new();
+        map = new Dictionary<string, List<(int, string)>>();
     }
     
     public void Set(string key, string value, int timestamp) {
-        if (!dict.ContainsKey(key)) dict.Add(key, new());
-        dict[key].Add((timestamp, value));
+        if(map.TryGetValue(key, out var list)) {
+            list.Add((timestamp, value));
+        } else {
+            var newList = new List<(int, string)>();
+            newList.Add((timestamp, value));
+            map.Add(key,newList);
+        }
     }
     
     public string Get(string key, int timestamp) {
-        if (!dict.ContainsKey(key)) return "";
-        int left = 0;
-        int right = dict[key].Count - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (dict[key][mid].Item1 > timestamp) 
-                right = mid - 1;
-            else left = mid + 1;
+        if(map.TryGetValue(key, out var list)) {
+           return BinarySearch(0, list.Count - 1, list, timestamp);
+        } else {
+            return string.Empty;
         }
-        if (right < 0) return "";
+    }
+    
+    private string BinarySearch(int start, int end, List<(int, string)> list, int target) {
+        var lastMid = -1;
         
-        return dict[key][right].Item2;
+        while(start <= end) {
+            var mid = (end - start) / 2 + start;
+            lastMid = mid;
+            
+            if(list[mid].Item1 == target) {
+                return list[mid].Item2;
+            } else if(list[mid].Item1 > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        
+        if(list.Count != 0 && end >= 0 && end < list.Count) return list[end].Item2;
+        
+        return string.Empty;
     }
 }
 
