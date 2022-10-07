fe4e9213f50903f72af497850e71efb5cf535153
Euael Eshete <euaelmeko@gmail.com>
2022-10-07 14:51:36 +0300

    Time: 2217 ms (6.62%), Space: 41.8 MB (13.55%) - LeetHub

--- 79-word-search/79-word-search.cs
+++ 79-word-search/79-word-search.cs
@@ -8,9 +8,9 @@ public class Solution {
         HashSet<(int, int)> path = new();
         
         bool rec(int row, int col, int ind) {
-            if (row >= n || row < 0 || col >= m || col < 0) return false;
+            if (row >= n || row < 0 || col >= m || col < 0 || board[row][col] != word[ind]) return false;
             
-            if (ind == word.Length) return true;
+            if (ind == word.Length - 1) return true;
             
             path.Add((row, col));
             
@@ -18,9 +18,8 @@ public class Solution {
             foreach (var (r, c) in Directions.Select(p => (p.Item1 + row, p.Item2 + col)))
             {
                 if (r >= n || r < 0 || c >= m || c < 0 || path.Contains((r, c))) continue;
-                if (!tmp && board[r][c] == word[ind])
+                if (!tmp)
                     tmp |= rec(r, c, ind + 1);
-                
             }
             
             path.Remove((row, col));
@@ -31,7 +30,7 @@ public class Solution {
         bool ans = false;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (!ans && board[i][j] == word[0]) ans = rec(i, j, 1);
+                if (!ans) ans = rec(i, j, 0);
             }
         }
         
