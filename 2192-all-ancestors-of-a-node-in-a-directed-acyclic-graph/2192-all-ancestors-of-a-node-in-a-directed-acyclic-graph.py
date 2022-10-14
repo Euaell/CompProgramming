a5d6e5293590dd8a7fc90037d970db6980374640
Euael Eshete <euaelmeko@gmail.com>
2022-10-14 15:53:17 +0300

    Time: 2945 ms (10.37%), Space: 32.4 MB (65.99%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -2,31 +2,22 @@ class Solution:
     def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
         adjList = defaultdict(list)
         
-        degree = [0] * n
         for e1, e2 in edges:
-            degree[e2] += 1
             adjList[e1].append(e2)
         
-        ans = [set() for _ in range(n)] 
+        ans = [[] for _ in range(n)] 
         
-        que = deque()
-        
-        
-        for i, d in enumerate(degree):
-            if d == 0: 
-                que.append(i)
-        
-        while que:
-            tmp = que.popleft()
+        def dfs(curr, par):
+            if curr != par: 
+                l = len(ans[curr])
+                if l == 0 or ans[curr][l - 1] != par: 
+                    ans[curr].append(par)
+                else: return
             
-            for r in adjList[tmp]:
-                ans[r].add(tmp)
-                ans[r] = ans[r].union(ans[tmp])
-                degree[r] -= 1
-                if degree[r] == 0:
-                    que.append(r)
+            for r in adjList[curr]:
+                dfs(r, par)
         
         for i in range(n):
-            ans[i] = sorted(ans[i])
+            dfs(i, i)
             
         return ans
