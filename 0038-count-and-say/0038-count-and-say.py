03566cd3222a6b212d4635f07e1cb3df92130872
Euael Eshete <euaelmeko@gmail.com>
2022-10-18 15:25:01 +0300

    Time: 110 ms (21.51%), Space: 14 MB (54.87%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,36 +1,22 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
-#         if n == 1:
-#             return "1"
+        if n == 1:
+            return "1"
         
-#         y = rec(n - 1)
+        y = self.countAndSay(n - 1)
         
-#         ans = ""
+        ans = ""
         
-#         i = 0
-#         while i < len(y):
-            
-        
-        def rec(x):
-            if x == 1:
-                return "1"
-            
-            y = rec(x - 1)
-            
-            ans = ""
-            
-            i = 0
-            while i < len(y):
+        i = 0
+        while i < len(y):
                 
-                j = i
-                while j < len(y) and y[j] == y[i]:
-                    j += 1
-                
-                count = j - i  
-                
-                ans += str(count) + str(y[i])
-                i = j
+            j = i
+            while j < len(y) and y[j] == y[i]:
+                j += 1
+
+            count = j - i  
+
+            ans += str(count) + str(y[i])
+            i = j
             
-            return ans
-        
-        return rec(n)
+        return ans
