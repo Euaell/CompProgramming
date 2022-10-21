80fc9fa12cc821874f57ddeb3cca3dea6d4a9002
Euael Eshete <euaelmeko@gmail.com>
2022-10-21 10:22:54 +0300

    Time: 374 ms (28.62%), Space: 14.8 MB (92.52%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.py
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.py
@@ -1,37 +1,36 @@
 class Solution:
     def smallestDistancePair(self, nums: List[int], k: int) -> int:
         n = len(nums)
+        
         nums.sort()
         
-        # get the min and max difference between all the pairs
+        maxDiff = nums[-1] - nums[0]
         minDiff = inf
-        maxDiff = nums[n - 1] - nums[0]
-        for i in range(0, n - 1):
-            diff = nums[i + 1] - nums[i]
-            minDiff = min(minDiff, diff)
+        for i in range(n - 1):
+            minDiff = min(minDiff, nums[i + 1] - nums[i])
         
-        # binary search  
+        # binary search
         left = minDiff
         right = maxDiff
+        ans = right
         while left <= right:
             mid = (right + left) // 2
             
+            # count the number of pair that have distance less or equal to mid
             greater = 0
-            
-            # sliding window
             l = 0
             for r in range(n):
+                
                 while nums[r] - nums[l] > mid:
                     l += 1
                 
+                # A single "l" can form l - r pairs, meaning with every element in the window except itself
                 greater += (r - l)
             
             if greater >= k:
+                ans = mid
                 right = mid - 1
             else:
                 left = mid + 1
         
-        return left
-        
-       
-            
+        return ans
