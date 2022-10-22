d0bbb3c2343e5ed4e5ab198d70a5268902882092
Euael Eshete <euaelmeko@gmail.com>
2022-10-22 14:32:29 +0300

    Time: 1425 ms (10.68%), Space: 65.9 MB (52.41%) - LeetHub

--- 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.py
+++ 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.py
@@ -14,18 +14,29 @@ class RandomizedCollection:
         if len(self.dict[val]) == 0:
             return False
         
+        # remove the last position of val in the array from the dictionary
         valIndex = self.dict[val].pop()
+        # get the last value
         lastVal = self.arr[-1]
-        
+            
+        # place the last value in the array at (position = valIndex)
+        # first add the valIndex to the set cause if they are some edge cases
         self.dict[lastVal].add(valIndex)
+        # remove the index it was at before by using DISCARD(instead of remove) cause it might have been deleted in the pop in the above code(which happens if there is only one element in the array)
         self.dict[lastVal].discard(len(self.arr) - 1)
         
+        # swap the position of the value to be deleted and the last value in the array
         self.arr[valIndex], self.arr[-1] = self.arr[-1], self.arr[valIndex]
+        # pop the value from the array
         self.arr.pop()
         return True
 
     def getRandom(self) -> int:
-        return choice(self.arr)
+        rnd = randint(0, len(self.arr) - 1)
+        return self.arr[rnd]
+    
+        # same as the above 2 line code
+        # return choice(self.arr)
 
 
 # Your RandomizedCollection object will be instantiated and called as such:
