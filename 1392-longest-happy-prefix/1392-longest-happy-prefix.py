26220800550314a6e0a7ffae6a4402c71115a769
Euael Eshete <euaelmeko@gmail.com>
2022-10-25 14:58:31 +0300

    Time: 566 ms (71.65%), Space: 18.7 MB (59.84%) - LeetHub

--- 1392-longest-happy-prefix/1392-longest-happy-prefix.py
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.py
@@ -6,15 +6,11 @@ class Solution:
         
         i = 0
         for j in range(1, n):
+            while i != 0 and s[i] != s[j]:
+                i = lps[i - 1]
+            
             if s[j] == s[i]:
                 lps[j] = i + 1
                 i += 1
-            else:
-                while i != 0 and s[i] != s[j]:
-                    i = lps[i - 1]
-                
-                if s[i] == s[j]:
-                    lps[j] = i + 1
-                    i += 1
-        print(lps)
+           
         return s[n - lps[-1]:]
