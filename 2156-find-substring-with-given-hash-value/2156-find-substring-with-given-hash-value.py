1992cbc02ebafad614fb257de88f771ac9744171
Euael Eshete <euaelmeko@gmail.com>
2022-10-25 21:32:20 +0300

    Time: 1466 ms (54.74%), Space: 14.2 MB (88.42%) - LeetHub

--- 2156-find-substring-with-given-hash-value/2156-find-substring-with-given-hash-value.py
+++ 2156-find-substring-with-given-hash-value/2156-find-substring-with-given-hash-value.py
@@ -3,9 +3,14 @@ class Solution:
         n = len(s)
         
         currHash = 0
+        
         t = k - 1
         right = n - 1
+        
         ans = ""
+        
+        lastPower = pow(power, k - 1, modulo)
+        
         for left in range(n - 1, -1, -1):
             currHash = (currHash + ((ord(s[left]) - 96) % modulo) * pow(power, t, modulo)) % modulo
             
@@ -15,10 +20,11 @@ class Solution:
                     ans = s[left: right + 1]
                 
                 #substract the right
-                currHash -= ((ord(s[right]) - 96) * pow(power, k - 1, modulo)) % modulo
+                currHash -= ((ord(s[right]) - 96) * lastPower) % modulo
                 currHash = (currHash * power) % modulo
                 right -= 1
             else:
+                # decrease the power until we find the right window length which is k
                 t -= 1
                 
         return ans
