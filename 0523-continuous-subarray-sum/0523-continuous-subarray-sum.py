b42095102d0940272a9619239d147009f6d339ba
Euael Eshete <euaelmeko@gmail.com>
2022-10-26 14:51:57 +0300

    Time: 2465 ms (35.00%), Space: 33.9 MB (12.72%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        n = len(nums)
+        dict = {}
+        sum = 0
+        dict[0] = -1
+        
+        for i in range(n):
+            sum += nums[i]
+            
+            if (sum % k) in dict:
+                if i - dict[(sum % k)] > 1: 
+                    print(nums[dict[(sum % k)] + 1: i + 1])
+                    return True
+            else:
+                dict[(sum % k)] = i
+        
+        return False
