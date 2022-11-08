110b8214d30bd59af197e909778c31e3ea3f5520
Euael Eshete <euaelmeko@gmail.com>
2022-11-08 22:01:42 +0300

    Time: 135 ms (45.01%), Space: 15.8 MB (51.68%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        # first find the xor of the two numbers
+        xor = 0
+        for num in nums:
+            xor ^= num
+        
+        # find the least significant bit of the xor
+        # # usually
+        # k = 0
+        # while k < 31:
+        #     # check if the kth bit is on starting from the right
+        #     if (1 << k) & xor == 1:
+        #         break
+        # 
+        # just perform a bitwise and with its negative
+        d = xor & -xor
+
+        # partition the array in two places
+        left = 0
+        right = 0
+
+        for num in nums:
+            if num & d == 0:
+                left ^= num
+            else:
+                right ^= num
+        
+        return [left, right]
+        
