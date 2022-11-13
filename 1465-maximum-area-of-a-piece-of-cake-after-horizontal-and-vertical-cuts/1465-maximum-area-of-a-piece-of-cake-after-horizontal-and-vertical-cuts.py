da7a6ae776b1663bf73bb512ddd75d69645d31b0
Euael Eshete <euaelmeko@gmail.com>
2022-11-13 19:28:13 +0300

    Time: 371 ms (80.29%), Space: 27.1 MB (46.60%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
@@ -3,17 +3,17 @@ class Solution:
         
         mod = 1_000_000_007
         
+        hCuts.append(h)
+        vCuts.append(w)
         hCuts.sort()
         vCuts.sort()
         
         maxH = hCuts[0]
         for i in range(1, len(hCuts)):
             maxH = max(maxH, hCuts[i] - hCuts[i - 1])
-        maxH = max(maxH, h - hCuts[-1])
         
         maxV = vCuts[0]
         for i in range(1, len(vCuts)):
             maxV = max(maxV, vCuts[i] - vCuts[i - 1])
-        maxV = max(maxV, w - vCuts[-1])
         
         return (maxV * maxH) % mod
