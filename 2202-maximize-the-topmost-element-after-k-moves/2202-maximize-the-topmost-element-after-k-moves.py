e649beb70bb12656d196187d36ed91b6ed44a591
Euael Eshete <euaelmeko@gmail.com>
2022-11-13 19:11:06 +0300

    Time: 1506 ms (40.19%), Space: 28 MB (55.14%) - LeetHub

--- 2202-maximize-the-topmost-element-after-k-moves/2202-maximize-the-topmost-element-after-k-moves.py
+++ 2202-maximize-the-topmost-element-after-k-moves/2202-maximize-the-topmost-element-after-k-moves.py
@@ -2,13 +2,13 @@ class Solution:
     def maximumTop(self, nums: List[int], k: int) -> int:
         n = len(nums)
         
-        if k > n:
-            # edge case
-            if n == 1 and k % 2 == 1:
-                return -1                
+        if n == 1 and k % 2 == 1:
+            return -1  
+        
+        if k > n:              
             return max(nums)
         
-        nums = nums + [-1]      
+        nums.append(-1)    
         
         left = [-1]
         if k - 1 > 0:
