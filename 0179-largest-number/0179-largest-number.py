3ce29901aa750d43fd2a5ebd3736b112537cfb7a
Euael Eshete <euaelmeko@gmail.com>
2023-01-18 22:28:44 +0300

    Time: 41 ms (80.55%), Space: 13.8 MB (64.06%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.py
@@ -0,0 +1,20 @@
+class Solution:
+    
+    def comp(self, x: str, y: str):
+        a = x + y
+        b = y + x
+        for i in range(len(a)):
+            if a[i] != b[i]:
+                return ord(b[i]) - ord(a[i])
+        return len(y) - len(x)
+        
+    def largestNumber(self, nums: List[int]) -> str:
+        newArr = [str(d) for d in nums]
+        newArr_sorted = sorted(newArr, key=functools.cmp_to_key(self.comp))
+        
+        ans = "".join(newArr_sorted)
+        if ans[0] == '0':
+            return '0'
+        
+        return ans
+    
