4d5efc98b426fdeb4b9c3f0299a2ce219f0929d4
Euael Eshete <euaelmeko@gmail.com>
2023-02-10 10:26:32 +0300

    Time: 66 ms (57.01%), Space: 14 MB (62.31%) - LeetHub

---
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numOfPairs(self, nums: List[str], target: str) -> int:
+        n = len(nums)
+        
+        ans = 0
+        
+        for i in range(n):
+            
+            for j in range(n):
+                if j == i:
+                    continue
+                if nums[i] + nums[j] == target:
+                    ans += 1
+                
+        return ans
+        
