bb157731c7f60472ab16a508c482db31e9c8756c
Euael Eshete <euaelmeko@gmail.com>
2023-02-15 20:01:37 +0300

    Time: 32 ms (79.36%), Space: 13.9 MB (95.37%) - LeetHub

--- 0284-peeking-iterator/0284-peeking-iterator.py
+++ 0284-peeking-iterator/0284-peeking-iterator.py
@@ -1,24 +1,3 @@
-# Below is the interface for Iterator, which is already defined for you.
-#
-# class Iterator:
-#     def __init__(self, nums):
-#         """
-#         Initializes an iterator object to the beginning of a list.
-#         :type nums: List[int]
-#         """
-#
-#     def hasNext(self):
-#         """
-#         Returns true if the iteration has more elements.
-#         :rtype: bool
-#         """
-#
-#     def next(self):
-#         """
-#         Returns the next element in the iteration.
-#         :rtype: int
-#         """
-
 class PeekingIterator:
     def __init__(self, iterator):
         self.ind = iterator
