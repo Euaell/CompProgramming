6c24466d26df9ee9470fb55ffd5b23177a8b6011
Euael Eshete <euaelmeko@gmail.com>
2023-04-26 14:29:56 +0300

    Time: 39 ms (25.40%), Space: 13.8 MB (39.79%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,7 +1,7 @@
 class Solution:
     def addDigits(self, num: int) -> int:
         
-        while num // 10 != 0:
+        while num >= 10:
             num = (num // 10) + (num % 10)
             
         return num
