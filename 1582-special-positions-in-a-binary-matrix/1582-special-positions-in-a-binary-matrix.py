e47cd830ac5c25a3e192600cff08e66da79f95b6
Euael Eshete <euaelmeko@gmail.com>
2023-12-13 07:03:28 +0300

    Time: 163 ms (33.64%), Space: 16.3 MB (99.69%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.py
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.py
@@ -5,9 +5,8 @@ class Solution:
         
         for i in range(len(mat)):
             for j in range(len(mat[0])):
-                if mat[i][j] == 1:
-                    column_count[j] = 1 if j not in column_count else column_count[j] + 1
-                    row_count[i] = 1 if i not in row_count else row_count[i] + 1
+                column_count[j] = mat[i][j] if j not in column_count else column_count[j] + mat[i][j]
+                row_count[i] = mat[i][j] if i not in row_count else row_count[i] + mat[i][j]
         
         ans = 0
         for i in range(len(mat)):
