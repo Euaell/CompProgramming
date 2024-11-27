8ce85f028613bbac45dbaee9c36f09f7e6976bc0
Euael Eshete <euaelmeko@gmail.com>
2024-11-27 07:49:46 +0300

    Time: 7 ms (27.70%), Space: 18.8 MB (7.97%) - LeetHub

--- 0563-binary-tree-tilt/0563-binary-tree-tilt.py
+++ 0563-binary-tree-tilt/0563-binary-tree-tilt.py
@@ -5,28 +5,21 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def __init__(self):
+        self.sum = 0
+
+    def calc(self, node: Optional[TreeNode]) -> int:
+        if not node:
+            return 0
+        
+        left = self.calc(node.left)
+        right = self.calc(node.right)
+        
+        self.sum += abs(left - right)
+        
+        return left + right + node.val
+        
     def findTilt(self, root: Optional[TreeNode]) -> int:
+        self.calc(root)
         
-        def rec(node: Optional[TreeNode]) -> int:
-            if not node:
-                return 0
-            
-            left = rec(node.left)
-            right = rec(node.right)
-            s = left + right + node.val
-            node.val = abs(left - right)
-            
-            return s
-        
-        rec(root)
-        
-        def calc(node: Optional[TreeNode]):
-            if not node:
-                return 0
-            
-            left = calc(node.left)
-            right = calc(node.right)
-            
-            return node.val + left + right
-        
-        return calc(root)
+        return self.sum
